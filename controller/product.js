6447211d31d73ee552da9d47046d506b220aba6e
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-04-24 05:58:38 +0530

    add commit

--- controller/product.js
+++ controller/product.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
 // const data = JSON.parse(fs.readFileSync("db.json", "UTF-8"));
 // const products = data.products;
-const model=require('../model/model')
-const Product=model.Product;
+const model = require("../model/model");
+const Product = model.Product;
 
 module.exports.createProduct = async (req, res) => {
   try {
@@ -15,7 +15,7 @@ module.exports.createProduct = async (req, res) => {
 
     res.status(201).json(savedProduct); // Respond with the saved product
   } catch (error) {
-    if (error.name === 'ValidationError') {
+    if (error.name === "ValidationError") {
       // If the error is a Mongoose validation error, respond with 400 Bad Request and the error details
       return res.status(400).json({ error: error.message });
     }
@@ -24,47 +24,43 @@ module.exports.createProduct = async (req, res) => {
   }
 };
 
-
-module.exports.getAllProducts =async (req, res) => {
-  const products=await Product.find({price:{$lt:100}});
+module.exports.getAllProducts = async (req, res) => {
+  const products = await Product.find({ price: { $lt: 100 } });
   res.json(products);
 };
-module.exports.getProduct =async (req, res) => {
+module.exports.getProduct = async (req, res) => {
   const id = req.params.id;
   console.log(id);
-  const products=await Product.findById(id);
+  const products = await Product.findById(id);
   res.json(products);
 };
 
-module.exports.replaceProduct =async (req, res) => {
+module.exports.replaceProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-    const doc=await Product.findOneAndReplace({_id:id},req.body)
-    res.status(201).json(doc)
-  }
-  catch(err){
+  try {
+    const doc = await Product.findOneAndReplace({ _id: id }, req.body);
+    res.status(201).json(doc);
+  } catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
 };
-module.exports.updateProduct =async (req, res) => {
+module.exports.updateProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-    const doc=await Product.findOneAndUpdate({_id:id},req.body)
-    res.status(201).json(doc)
-  }
-  catch(err){
+  try {
+    const doc = await Product.findOneAndUpdate({ _id: id }, req.body);
+    res.status(201).json(doc);
+  } catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
 };
-module.exports.deleteProduct =async (req, res) => {
+module.exports.deleteProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-    const doc=await Product.findOneAndDelete({_id:id})
-    res.status(201).json(doc)
-  }
-  catch(err){
+  try {
+    const doc = await Product.findOneAndDelete({ _id: id });
+    res.status(201).json(doc);
+  } catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
